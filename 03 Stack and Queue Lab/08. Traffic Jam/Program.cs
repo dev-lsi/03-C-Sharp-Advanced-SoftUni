7ed8b45e3541f172012693e8b3b9e663bd47cbdc
LachezarIvanov-git <118828142+LachezarIvanov-git@users.noreply.github.com>
2023-02-12 18:15:27 +0200

    problem 08 "traffic jam" fixed

--- 03 Stack and Queue Lab/08. Traffic Jam/Program.cs
+++ 03 Stack and Queue Lab/08. Traffic Jam/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _08._Traffic_Jam
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n=int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>();
+            List<string> list = new List<string>();
+            int counter = 0;
+
+            while (true) 
+            {
+                string input = Console.ReadLine();
+                
+                if (input == "end")
+                {
+                    Console.WriteLine($"{counter} cars passed the crossroads.");
+                    return;
+                }
+                else if(input=="green")
+                {
+                    int passes = 0;
+                     
+                    if (queue.Count >= n)
+                    {
+                        passes = n;
+                    }
+                    else
+                    {
+                        passes = queue.Count;
+                    }
+                    for (int i = 0; i < passes; i++)
+                    {
+                        Console.WriteLine($"{queue.Dequeue()} passed!");
+                        counter++;
+                    }
+                    continue;
+                }
+                else
+                {
+                   queue.Enqueue(input);
+                }
+            }
+
+            
         }
     }
 }
